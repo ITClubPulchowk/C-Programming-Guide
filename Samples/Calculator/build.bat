6f71320b6d3fda3cabcbf8b1817d8cce2581fcc9
TilakChad <49703944+TilakChad@users.noreply.github.com>
2021-04-27 06:52:04 +0545

    Update build.bat

--- Samples/Calculator/build.bat
+++ Samples/Calculator/build.bat
@@ -3,4 +3,4 @@
 if not exist "./bin" (md bin)
 @echo on
 
-gcc -I./includes/ ./src/main.c ./src/parser.c ./src/regex.c ./src/symbol_table.c ./src/token.c -o calculator
+gcc -I./includes/ ./src/main.c ./src/parser.c ./src/regex.c ./src/symbol_table.c ./src/token.c -o ./bin/calculator
