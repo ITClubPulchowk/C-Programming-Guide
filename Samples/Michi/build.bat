4860c8384c9d522c9c27bb64da4a22719635b80d
Ashish Bhattarai <zeroa5620@gmail.com>
2021-04-28 19:28:40 +0545

    changed from UTF-16 to UTF-8, oof vs

--- Samples/Michi/build.bat
+++ Samples/Michi/build.bat
@@ -73,7 +73,7 @@ call xcopy "resource.rc" bin\ClangBuild /Y
 call xcopy "Logo.ico" bin\ClangBuild /Y
 call xcopy "Logo.bmp" bin\ClangBuild /Y
 pushd bin\ClangBuild
-call llvm-rc resource.h
+call llvm-rc %ResourceFileIn%
 call clang -Wno-switch -Wno-pointer-sign -Wno-enum-conversion -D_CRT_SECURE_NO_WARNINGS -D_GLFW_WIN32 %CLANGFlags% %SourceFiles% %ResourceFileOut% -o Michi.exe -luser32.lib -lgdi32.lib -lshell32.lib -lopengl32.lib
 popd
 echo -------------------------------------
@@ -95,7 +95,7 @@ call xcopy "resource.rc" bin\GccBuild /Y
 call xcopy "Logo.ico" bin\GccBuild /Y
 call xcopy "Logo.bmp" bin\GccBuild /Y
 pushd bin\GccBuild
-call windres -i resource.h -o resource.o
+call windres -i %ResourceFileIn% -o resource.o
 call gcc -Wno-switch -Wno-pointer-sign -Wno-enum-conversion -D_CRT_SECURE_NO_WARNINGS -D_GLFW_WIN32 %GCCFlags% %SourceFiles% resource.o -o Michi.exe -luser32.lib -lgdi32.lib -lshell32.lib -lopengl32.lib
 popd
 echo -------------------------------------
