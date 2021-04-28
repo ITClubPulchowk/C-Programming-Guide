c0684b067bf1fc9ff1066a056e724ea3189cbb93
Ashish Bhattarai <zeroa5620@gmail.com>
2021-04-28 15:55:34 +0545

    gcc build.bat

--- Samples/Michi/build.bat
+++ Samples/Michi/build.bat
@@ -95,8 +95,8 @@ call xcopy "resource.rc" bin\GccBuild /Y
 call xcopy "Logo.ico" bin\GccBuild /Y
 call xcopy "Logo.bmp" bin\GccBuild /Y
 pushd bin\GccBuild
-call windres -i %ResourceFileIn% -o %ResourceFileOut%
-call gcc -Wno-switch -Wno-pointer-sign -Wno-enum-conversion -D_CRT_SECURE_NO_WARNINGS -D_GLFW_WIN32 %GCCFlags% %SourceFiles% %ResourceFileOut% -o Michi.exe -luser32.lib -lgdi32.lib -lshell32.lib -lopengl32.lib
+call windres -i %ResourceFileIn% -o resource.o
+call gcc -Wno-switch -Wno-pointer-sign -Wno-enum-conversion -D_CRT_SECURE_NO_WARNINGS -D_GLFW_WIN32 %GCCFlags% %SourceFiles% resource.o -o Michi.exe -luser32.lib -lgdi32.lib -lshell32.lib -lopengl32.lib
 popd
 echo -------------------------------------
 goto Finished
