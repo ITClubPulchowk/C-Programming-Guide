84a5f09974a78d4bf513d7e92d00f965088df270
suyog <suyogsatyal45@gmail.com>
2021-06-10 21:21:41 +0545

    Added all the resources of day 0 of the c workshop

---
+++ Workshop Resources/Day0/codes/four.c
@@ -0,0 +1,11 @@
+int main(){
+    int birthYear, age;
+    int drinkingAge;
+    birthYear = 2015;
+    age = 2021 - birthYear;
+    if (age <= 18)
+        drinkingAge = 0;
+    else
+        drinkingAge = 1;
+    return 0;
+}
---
+++ Workshop Resources/Day0/codes/one.c
@@ -0,0 +1,11 @@
+
+int main()
+{
+    int a=5, c=2;
+    float d;
+    a = 8;
+    d = 2.5;
+    float b = 'a'/2.5f;
+    char z = 'A' + 32;
+    return 0;
+}
